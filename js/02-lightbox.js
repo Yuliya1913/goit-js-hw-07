de3ea7ec4f9070a66928dd46ddb318910b7c3c1a
Yuliya1913 <Lera-855@email.ua>
2023-03-04 17:53:17 +0200

    add tasks

---
+++ js/02-lightbox.js
@@ -0,0 +1,35 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+// получим ссылку на div с классом gallery
+const galleryDivEl = document.querySelector('.gallery');
+console.log(galleryDivEl);
+
+
+// создадим функцию для создания карточки одного изображения
+
+const creatGalerry = ({ preview, original, description }) =>
+   
+`<div class="gallery__item">
+    <a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a>
+</div>`;
+      
+// переберем массив объектов и на каждый из них вызовем функцию для создания карточки
+
+const newArrayGalerry = galleryItems.map(el => creatGalerry(el)).join("");
+console.log(newArrayGalerry);
+
+// Добавим в HTML полученную разметку с карточками
+
+galleryDivEl.insertAdjacentHTML('afterbegin', newArrayGalerry);
+
+let gallery = new SimpleLightbox('.gallery a', {
+    captionsData: "alt",
+    captionDelay: 250,
+    
+})
+
+console.log(gallery);
+console.log(galleryItems);
